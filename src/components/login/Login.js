a15ec094a23ebc7cca2aaaff9878959c6ab52d5e
biorubenfs <biorubenfs@hotmail.com>
2021-04-14 09:50:51 +0200

    Component modified to get user inputs

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,14 +1,43 @@
 import './Login.css';
+import React, { Fragment, useState } from 'react';
 
 const Login = (props) => {
 
+    const [data, setData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const getInputChange = (event) => {
+        console.log(event);
+        console.log(event.target.value);
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        })
+    }
+
     return (
         <div>
+            <form className="login-form container">
+                <input
+                    className="input"
+                    type="email"
+                    placeholder="Enter your email"
+                    name="email"
+                    onChange={getInputChange}>
+                </input><br></br>
 
-            <input className="input" type="email" placeholder="Enter your email"></input><br></br>
-            <input className="input" type="password" placeholder="Enter your password"></input><br></br>
-            <button className="login-button">Login</button>
+                <input
+                    className="input"
+                    type="password"
+                    placeholder="Enter your password"
+                    name="password"
+                    onChange={getInputChange}>
+                </input><br></br>
 
+                <button className="login-button" type="submit">Login</button>
+            </form>
         </div>
     )
 }
