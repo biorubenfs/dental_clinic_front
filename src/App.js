3ceb2fdbea7db5bbd709e37a5d55f067a76abf7b
TheJarki <melendez.jarki@gmail.com>
2021-04-17 10:43:05 +0200

    match app.js to develop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+// import logo from './logo.svg';
 import './App.css';
 import Home from './containers/home/Home';
+import NewAppointment from './containers/newAppointment/NewAppointment';
+import Login from './containers/login/Login'
+import ViewAppointments from './containers/viewAppointments/ViewAppointments';
+import Signup from './containers/signup/Signup';
+import logo from './logo.png';
 
 function App() {
-
   return (
-    <div>
-      <Home></Home>
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body className="App-body">
+        <img src={logo} className="App-logo" alt="logo" />
+        <NewAppointment></NewAppointment>
+        {/*<ViewAppointments></ViewAppointments>*/}
+        <Signup></Signup>
+        {/* <Login></Login> */}
+        {/* <Home></Home> */}
+      </body>
+      <footer className="App-footer"></footer>
     </div>
   );
-  
 }
 
 export default App;
