dc6eb458f96e6ff47d39c0015f767419d044e6d1
TheJarki <melendez.jarki@gmail.com>
2021-04-17 22:49:56 +0200

    responsive appointment cards

--- src/components/appointmentCard/AppointmentCard.js
+++ src/components/appointmentCard/AppointmentCard.js
@@ -7,37 +7,40 @@ const AppointmentCard = (props) => {
         <div className="card">
             <div className="row">
                 <div className="column">
-                    <div className="field">
+                    <div className="field client">
                         <strong>Client</strong>
                     </div>
-                    <div className="field">
+                    <div className="field email">
                         <strong>Email</strong>
                     </div>
-                    <div className="field">
+                    <div className="field doctor">
                         <strong>Doctor</strong>
-                    </div>                    
-                    <div className="field">
+                    </div>
+                    <div className="field speciality">
                         <strong>Speciality</strong>
-                    </div>                    
+                    </div>
                 </div>
                 <div className="column">
-                <div className="field">
+                    <div className="field client">
                         {props.clientName}
                     </div>
-                    <div className="field">
+                    <div className="field email">
                         {props.email}
                     </div>
-                    <div className="field">
+                    <div className="field doctor">
                         {props.doctorName}
                     </div>
-                    <div className="field">
+                    <div className="field speciality">
                         {props.speciality}
                     </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="appointment-id">
-                    ID: {props.id}
+            <div className="pivot">
+                <div className="id-info">
+                    <div className="id-label">ID:</div>
+                    <div className="appointment-id">
+                        {props.id}
+                    </div>
                 </div>
                 <div className="date">
                     {props.date}
