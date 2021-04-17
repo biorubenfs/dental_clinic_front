a196a29893988464fdd4c71782584baf78a28481
biorubenfs <biorubenfs@hotmail.com>
2021-04-17 15:33:40 +0200

    Added provisional footer with icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,11 +3,22 @@ import './Footer.css'
 const Footer = () => {
 
     return (
-        <div>
-            Footer
+        <div className="footer-bg">
+            <div className="footer">
+                <div>
+                    <div className="footer-title">Jarki and Ruben Team</div>
+                    <div className="footer-paragraph">Copyright © 2021</div>
+                </div>
+
+                <div>70 Great Russell St, Holborn, London WC1B 3BN, UK</div>
+                <div className="footer-iconos">
+                    <a className="icon icon-twitter" target="_blank" href="http://www.google.es"></a>
+                    <a className="icon icon-instagram" target="_blank" href="http://www.google.es"></a>
+                    <a className="icon icon-facebook" target="_blank" href="http://www.google.es"></a>
+                </div>
+            </div>
         </div>
     )
-
 }
 
 export default Footer
