3088cc2d9be046b005138fb6491bef40886fd704
biorubenfs <biorubenfs@hotmail.com>
2021-04-17 13:44:21 +0200

    Navbar added with responsive design

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,19 +1,15 @@
-import './Navbar.css'
-import logo from '../../logo.png'
+import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div className='navbar'>
-            <div className='logo' >
-                <img src={logo} alt='logo'></img>
-            </div>
-            <div className='navlink'>
+        <div className='navbar-bg'>
+            <nav className='navbar'>
                 <a href='http://localhost:3000/'>Treatments</a>
                 <a href='http://localhost:3000/'>Get an Appointment</a>
                 <a href='http://localhost:3000/'>Contact</a>
                 <a href='http://localhost:3000/'>Login</a>
                 <a href='http://localhost:3000/'>Register</a>
-            </div>
+            </nav>
         </div>
     )
 }
