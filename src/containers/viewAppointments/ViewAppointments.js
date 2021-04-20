b20b364b693c79be302f48d1ca7d904532152149
TheJarki <melendez.jarki@gmail.com>
2021-04-20 16:08:44 +0200

    removed experimental pagination

--- src/containers/viewAppointments/ViewAppointments.js
+++ src/containers/viewAppointments/ViewAppointments.js
@@ -9,9 +9,6 @@ const ViewAppointments = () => {
 
     const [error, setError] = useState(null);
     const [results, setResults] = useState(null);
-    const [count, setCount] = useState(null);
-    const [startSkip, setStartSkip] = useState(0);
-    const [endSkip, setEndtSkip] = useState(10);
     const [alert, setAlert] = useState(false);
     const [cancelId, setCancelId] = useState(null);
 
@@ -26,13 +23,9 @@ const ViewAppointments = () => {
             const res = await fetchAppointments.appointmentStatus();
             const json = await res.json();
             const { rows } = json;
-            const { count } = json;
-
-            const tenResults = rows.slice(startSkip, endSkip);
 
             if (rows) {
-                setResults(tenResults);
-                setCount(Math.ceil(count / 10));
+                setResults(rows);
             }
 
         } catch (e) {
@@ -45,18 +38,6 @@ const ViewAppointments = () => {
 
     if (error === 0) {
         msg = <AppointmentMessage msg="Internal server error"></AppointmentMessage>
-    }
-
-    const nextPage = () => {
-        setStartSkip(startSkip + 10);
-        setEndtSkip(endSkip + 10);
-        getAppointments();
-    }
-
-    const previousPage = () => {
-        setStartSkip(startSkip - 10);
-        setEndtSkip(endSkip - 10);
-        getAppointments();
     }
 
     const cancelDate = async () => {
@@ -75,17 +56,12 @@ const ViewAppointments = () => {
                         {results.map(element => <AppointmentCard key={results.indexOf(element)} id={element.id}
                             date={new Date(element.date).toDateString() + ' ' + new Date(element.date).toLocaleTimeString()}
                             status={element.status} clientName={element.User.name} doctorName={element.Doctor.name}
-                            email={element.User.email} speciality={element.Doctor.speciality} 
+                            email={element.User.email} speciality={element.Doctor.speciality}
                             cancelBtn={() => {
                                 setAlert(true);
                                 setCancelId(element.id);
                             }}></AppointmentCard>)}
                     </div>}
-                </div>
-                <div className="pagination">
-                    <button className="page-btn" href="#" onClick={() => previousPage()}>prev</button>
-                    <div>page 1 of {count}</div>
-                    <button className="page-btn" href="#" onClick={() => nextPage()}>next</button>
                 </div>
             </div>
         </>
